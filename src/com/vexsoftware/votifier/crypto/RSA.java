be4e615c7749cdc9ccbec51573de1b7d5eaba432
Adam <packetdev@gmail.com>
2015-11-15 18:40:15 -0500

    Remove unused method

--- src/com/vexsoftware/votifier/crypto/RSA.java
+++ src/com/vexsoftware/votifier/crypto/RSA.java
@@ -19,7 +19,6 @@
 package com.vexsoftware.votifier.crypto;
 
 import java.security.PrivateKey;
-import java.security.PublicKey;
 
 import javax.crypto.Cipher;
 
@@ -30,23 +29,6 @@ import javax.crypto.Cipher;
  * @author Blake Beaupain
  */
 public class RSA {
-
-	/**
-	 * Encrypts a block of data.
-	 * 
-	 * @param data
-	 *            The data to encrypt
-	 * @param key
-	 *            The key to encrypt with
-	 * @return The encrypted data
-	 * @throws Exception
-	 *             If an error occurs
-	 */
-	public static byte[] encrypt(byte[] data, PublicKey key) throws Exception {
-		Cipher cipher = Cipher.getInstance("RSA");
-		cipher.init(Cipher.ENCRYPT_MODE, key);
-		return cipher.doFinal(data);
-	}
 
 	/**
 	 * Decrypts a block of data.
